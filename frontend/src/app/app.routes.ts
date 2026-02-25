32fe5ca38cebe530492bf793dbd03c90957af121
randriamanantenavony <152492886+randriamanantenavony@users.noreply.github.com>
2026-02-25 16:37:58 +0300

    insertion ventes
    insertion de ventes pour un magasin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ShopList } from './pages/shop-list/shop-list';
 import { ShopProducts } from './pages/shop-products/shop-products';
 import { ShopProductCreate } from './pages/shop-product-create/shop-product-create';
 import { PurchaseFormComponent } from './pages/purchases/purchases';
+import { SaleFormComponent } from './pages/sales/sales';
 
 export const routes: Routes = [
     {path : 'login', component : Dashboard},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path : 'products-boutiques', component : ShopProducts},
     {path : 'create-products', component : ShopProductCreate},
     {path : 'purchase', component : PurchaseFormComponent},
+    {path : 'sales', component : SaleFormComponent},
     {path :'', redirectTo: '/login', pathMatch : 'full'}
 ];
