5160e28b467e7819f09c59d050a999cff09dad60
Andriamihaja <andoandria2386@gmail.com>
2026-02-26 10:11:40 +0300

    Revert "resolution conflit"
    This reverts commit 91cd40f48ddec3ac4a587e22f75efcc455dbbd27.

--- frontend/src/app/shared/constants/api-endpoints.ts
+++
@@ -1,8 +0,0 @@
-export const ApiEndpoints = {
-  USER: {
-    LOGIN: 'api/auth/login'
-  },
-  LOTS:{
-    GETALL:'api/lots/'
-  }
-};
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://your-prod-api.com/',
-};
